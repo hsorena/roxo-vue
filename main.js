fcb499aae9c33157e49b97cb1ef69cb4631966f0
sorena <ha.amini74@gmail.com>
2021-06-29 22:05:33 +0430

    Clean doc

--- main.js
+++ main.js
@@ -2,41 +2,7 @@
 new Vue({
     el:"#root",
     data:{
-        link:'http://www.google.com',
-        linkTitle:'Google',
-        attachRed:false,
-        color:"green",
-        width:100,
-        bgColor:'pink'
+        
     }
-    ,
-    methods:{
-        //old version
-        oldChangeLink:function () {
-            this.link = 'http://www.soft98.ir'
-            this.linkTitle = 'Soft98'
-        }
-        ,
-        //new version
-        changeLink(){
-            this.link = 'http://www.soft98.ir'
-            this.linkTitle = 'Soft98'
-        }
-    }
-    ,
-    computed:{
-        divClasses(){
-            return{
-                red: this.attachRed ,
-                blue: !this.attachRed
-            }
-        }
-        ,
-        divStyles(){
-            return{
-                backgroundColor : this.bgColor,
-                width: this.width + 'px'
-            }
-        }
-    }
+
 })
