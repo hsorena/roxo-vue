f18de1b748e6c5ed1b73746768b44fb0249a92a2
sorena <ha.amini74@gmail.com>
2021-06-29 19:03:43 +0430

    Dynamic Classes And Styles

--- main.js
+++ main.js
@@ -2,5 +2,41 @@
 new Vue({
     el:"#root",
     data:{
+        link:'http://www.google.com',
+        linkTitle:'Google',
+        attachRed:false,
+        color:"green",
+        width:100,
+        bgColor:'pink'
+    }
+    ,
+    methods:{
+        //old version
+        oldChangeLink:function () {
+            this.link = 'http://www.soft98.ir'
+            this.linkTitle = 'Soft98'
+        }
+        ,
+        //new version
+        changeLink(){
+            this.link = 'http://www.soft98.ir'
+            this.linkTitle = 'Soft98'
+        }
+    }
+    ,
+    computed:{
+        divClasses(){
+            return{
+                red: this.attachRed ,
+                blue: !this.attachRed
+            }
+        }
+        ,
+        divStyles(){
+            return{
+                backgroundColor : this.bgColor,
+                width: this.width + 'px'
+            }
+        }
     }
 })
