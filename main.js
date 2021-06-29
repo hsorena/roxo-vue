0d6639f6be59990c2b42a2a6c33e0542ad374757
sorena <ha.amini74@gmail.com>
2021-06-29 22:48:32 +0430

    Conditions and loop in vue

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 new Vue({
     el:"#root",
     data:{
-        
+        show:true,
+        foods:['sandwich' , 'pizza' , 'kebab'],
+        newFood:'',
+        persons:[
+            {name:'hasan' , age:25 , email:'hasan@gmail.com'},
+            {name:'hosein' , age:28 , email:'hosein@gmail.com'}
+        ]
     }
 
 })
