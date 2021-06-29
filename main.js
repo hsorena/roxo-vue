08dfc2ffa088fd1eb929eabce358532a1b22a602
sorena <ha.amini74@gmail.com>
2021-06-29 22:49:18 +0430

    Clean doc

--- main.js
+++ main.js
@@ -2,13 +2,7 @@
 new Vue({
     el:"#root",
     data:{
-        show:true,
-        foods:['sandwich' , 'pizza' , 'kebab'],
-        newFood:'',
-        persons:[
-            {name:'hasan' , age:25 , email:'hasan@gmail.com'},
-            {name:'hosein' , age:28 , email:'hosein@gmail.com'}
-        ]
+
     }
 
 })
