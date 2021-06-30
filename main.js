492a411088de4fbc0431d1f4c7d8ed42e4269dc0
sorena <ha.amini74@gmail.com>
2021-06-30 22:06:06 +0430

    Define two instance of vue in project and use setTimeOut function

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
 
-new Vue({
-    el:"#root",
+var vm1 = new Vue({
+    el:"#app1",
     data:{
-
+        title : 'The vueJs instance',
+        showParagraph:false
+    },
+    methods:{
+        show(){
+            this.showParagraph = true;
+            this.updateTitle('The VueJS Instance (Updated)');
+        },
+        updateTilte(title){
+            this.title = title;
+        }
+    },
+    computed:{
+        lowerCaseTitle(){
+            return this.title.toLowerCase();
+        }
+    },
+    watch:{
+        title(value){
+            alert('Title changed, new value : ' + value);
+        }
     }
 
+})
+
+setTimeout(function () {
+    vm1.title = 'Title changed by timer'
+},10000)
+
+var vm2 = new Vue({
+    el:'#app2',
+    data: {
+        secondTitle:'The second title from vm2'
+    },
+    methods: {
+        changeTitle(){
+            vm1.title = 'Title Changed from vm2'
+        }
+    }
 })
