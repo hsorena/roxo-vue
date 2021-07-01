36d854a5ae265b1f1256d51db5e8fee9780db329
sorena <ha.amini74@gmail.com>
2021-07-01 16:40:34 +0430

    clean doc

--- main.js
+++ main.js
@@ -2,36 +2,7 @@
 var vm1 = new Vue({
     el:"#app1",
     data:{
-        title :'Lifecycle'
-    },
-    beforeCreate(){
-        console.log('beforeCreate')
-    },
-    created(){
-        console.log('created')
-    },
-    beforeMount(){
-        console.log('beforeMount')
-    },
-    mounted(){
-        console.log('mounted')
-    },
-    beforeUpdate(){
-        console.log('beforeUpdate')
-    },
-    updated(){
-        console.log('updated')
-    },
-    beforeDestroy(){
-        console.log('beforeDestroy')
-    },
-    destroyed(){
-        console.log('destroyed')
-    },
-    methods:{
-        changeDestroy(){
-            this.$destroy()
-        }
+
     }
 
 })
