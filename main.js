66cc7b7a5cd50d9f16cb602f4ba46bd3bbc6328f
sorena <ha.amini74@gmail.com>
2021-07-01 16:34:15 +0430

    All Available hooks in vue js

--- main.js
+++ main.js
@@ -2,6 +2,36 @@
 var vm1 = new Vue({
     el:"#app1",
     data:{
+        title :'Lifecycle'
     },
+    beforeCreate(){
+        console.log('beforeCreate')
+    },
+    created(){
+        console.log('created')
+    },
+    beforeMount(){
+        console.log('beforeMount')
+    },
+    mounted(){
+        console.log('mounted')
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate')
+    },
+    updated(){
+        console.log('updated')
+    },
+    beforeDestroy(){
+        console.log('beforeDestroy')
+    },
+    destroyed(){
+        console.log('destroyed')
+    },
+    methods:{
+        changeDestroy(){
+            this.$destroy()
+        }
+    }
 
 })
